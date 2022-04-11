a20d55d330226784b531d748f1bb3156016ea1e5
Nikola Whallon <nikola@deepgram.com>
2022-04-11 14:05:35 -0700

    converting message types

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::ws::{Message, WebSocket, WebSocketUpgrade},
+    extract::ws::{WebSocket, WebSocketUpgrade},
     response::IntoResponse,
     routing::get,
     Router,
@@ -12,6 +12,7 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::WebSocketStream;
 use tokio_tungstenite::MaybeTlsStream;
+use std::convert::From;
 
 #[tokio::main]
 async fn main() {
@@ -53,7 +54,7 @@ async fn from_client(
             return;
         };
 
-        if destination_sender.send(tokio_tungstenite::tungstenite::Message::Text("test".to_string())).await.is_err() {
+        if destination_sender.send(Message::from(msg).into()).await.is_err() {
             // client disconnected
             return;
         }
@@ -61,7 +62,7 @@ async fn from_client(
 }
 
 async fn to_client(
-    mut this_sender: SplitSink<WebSocket, Message>,
+    mut this_sender: SplitSink<WebSocket, axum::extract::ws::Message>,
     mut destination_receiver: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>,
 ) {
     while let Some(msg) = destination_receiver.next().await {
@@ -72,9 +73,71 @@ async fn to_client(
             return;
         };
 
-        if this_sender.send(axum::extract::ws::Message::Text("test".to_string())).await.is_err() {
+        if this_sender.send(Message::from(msg).into()).await.is_err() {
             // client disconnected
             return;
+        }
+    }
+}
+
+pub enum Message {
+    Text(String),
+    Binary(Vec<u8>),
+    Ping(Vec<u8>),
+    Pong(Vec<u8>),
+    Close(Option<tungstenite::protocol::CloseFrame<'static>>),
+}
+
+impl From<axum::extract::ws::Message> for Message {
+    fn from(item: axum::extract::ws::Message) -> Self {
+        match item {
+            axum::extract::ws::Message::Text(text) => Message::Text(text),
+            axum::extract::ws::Message::Binary(binary) => Message::Binary(binary),
+            axum::extract::ws::Message::Ping(ping) => Message::Ping(ping),
+            axum::extract::ws::Message::Pong(pong) => Message::Pong(pong),
+            // will deal with this later
+            axum::extract::ws::Message::Close(close) => Message::Close(None),
+        }
+    }
+}
+
+impl From<tokio_tungstenite::tungstenite::Message> for Message {
+    fn from(item: tokio_tungstenite::tungstenite::Message) -> Self {
+        match item {
+            tokio_tungstenite::tungstenite::Message::Text(text) => Message::Text(text),
+            tokio_tungstenite::tungstenite::Message::Binary(binary) => Message::Binary(binary),
+            tokio_tungstenite::tungstenite::Message::Ping(ping) => Message::Ping(ping),
+            tokio_tungstenite::tungstenite::Message::Pong(pong) => Message::Pong(pong),
+            // will deal with this later
+            tokio_tungstenite::tungstenite::Message::Close(close) => Message::Close(None),
+        }
+    }
+}
+
+// why the hell is this needed
+impl From<Message> for axum::extract::ws::Message {
+    fn from(item: Message) -> axum::extract::ws::Message {
+        match item {
+            Message::Text(text) => axum::extract::ws::Message::Text(text),
+            Message::Binary(binary) => axum::extract::ws::Message::Binary(binary),
+            Message::Ping(ping) => axum::extract::ws::Message::Ping(ping),
+            Message::Pong(pong) => axum::extract::ws::Message::Pong(pong),
+            // will deal with this later
+            Message::Close(close) => axum::extract::ws::Message::Close(None),
+        }
+    }
+}
+
+// why the hell is this needed
+impl From<Message> for tokio_tungstenite::tungstenite::Message {
+    fn from(item: Message) -> tokio_tungstenite::tungstenite::Message {
+        match item {
+            Message::Text(text) => tokio_tungstenite::tungstenite::Message::Text(text),
+            Message::Binary(binary) => tokio_tungstenite::tungstenite::Message::Binary(binary),
+            Message::Ping(ping) => tokio_tungstenite::tungstenite::Message::Ping(ping),
+            Message::Pong(pong) => tokio_tungstenite::tungstenite::Message::Pong(pong),
+            // will deal with this later
+            Message::Close(close) => tokio_tungstenite::tungstenite::Message::Close(None),
         }
     }
 }
